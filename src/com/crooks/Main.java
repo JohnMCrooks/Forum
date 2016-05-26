13e6ebb0bcb8ff01e1e4d5ec1eea3338bfa45efa
John Crooks <johnmcrooks@gmail.com>
2016-05-26 11:43:07 -0400

    Week 2 Day 4 - In Class Work Refactored Main.java for a cleaner read

--- src/com/crooks/Main.java
+++ src/com/crooks/Main.java
@@ -7,11 +7,9 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) throws FileNotFoundException {
+    public static ArrayList<Post> parseFile(String fileName) throws FileNotFoundException {
         ArrayList<Post> posts = new ArrayList<Post>();
-
-        //Parse file
-        File f = new File("posts.txt");
+        File f = new File(fileName);
         Scanner fileScanner = new Scanner(f);
         while  (fileScanner.hasNext()){
             String line = fileScanner.nextLine(); // store each line into a variable
@@ -19,21 +17,32 @@ public class Main {
             System.out.println( " ");
             Post post = new Post(Integer.valueOf(columns[0]),columns[1],columns[2]);  //Create Class object with freshly seperated variables
             posts.add(post);
-
         }
+        return posts;
+    }
+
+    public static void printPosts(ArrayList<Post> posts, int currentpost){
+        //print out replies to the current post
+        int postID = 0;
+        for (Post post : posts){
+            if (post.replyID== currentpost){
+                System.out.printf("[%s] %s by %s.\n",postID,post.text,post.author);
+            }
+            postID++;
+        }
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+        //Parse file
+        ArrayList<Post> posts = parseFile("posts.txt");
 
         //Start Main Loop
         Scanner consoleScanner = new Scanner(System.in);
         int currentpost = -1;
         while (true) {
-            //print out replies to the current post
-            int postID = 0;
-            for (Post post : posts){
-                if (post.replyID== currentpost){
-                    System.out.printf("[%s] %s by %s.\n",postID,post.text,post.author);
-                }
-                postID++;
-            }
+
+            printPosts(posts, currentpost);
             // ask for new ID
             System.out.println("Type id you want to see replies to: ");
             currentpost = Integer.valueOf(consoleScanner.nextLine());
